5703dbf98e50df739f217b3fa589f05545691bd9
zakariabouhaik <contact.bouhaikzakaria@gmail.com>
2024-07-17 23:11:10 +0100

    Initial commit

---
+++ src/main/java/com/example/rambackend/RamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.rambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RamBackendApplication.class, args);
+    }
+
+}
