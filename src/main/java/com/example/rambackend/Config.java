a60d31b10ba08481a8594d94a0eb5b12952e423d
zakariabouhaik <contact.bouhaikzakaria@gmail.com>
2024-07-25 17:00:25 +0100

    commit
    commit

---
+++ src/main/java/com/example/rambackend/Config.java
@@ -0,0 +1,22 @@
+package com.example.rambackend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:3000"); // Autoriser les requêtes depuis localhost:3000
+        config.addAllowedMethod("*"); // Autoriser toutes les méthodes HTTP (GET, POST, PUT, DELETE, etc.)
+        config.addAllowedHeader("*"); // Autoriser tous les en-têtes
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
